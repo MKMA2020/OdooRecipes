0504ac8b1d5e4ab62675ebd606d7e72631607050
2dam <2dam@MACINF01.TartangaLH.eus>
2020-11-30 14:23:28 +0100

    Model classes created.

--- Desktop/Odoo/recipes/models/ingredient.py
+++ Desktop/Odoo/recipes/models/ingredient.py
@@ -1,4 +1,11 @@
-from odoo import models
+from odoo import models, fields
 
 class Ingredient(models.model):
     __name = 'recipes.ingredient'
+    
+    id = fields.Integer(requiered=True),
+    name = fields.char(requered=True),
+    
+    type = fields.selection((('n','Unconfirmed'), ('c','Confirmed')),'Tipo'),
+    
+    
