b090b9fac3e2cfba7a2e76f74dae00f3b6c4bed9
2dam <2dam@MACINF02.TartangaLH.eus>
2020-12-14 12:58:42 +0100

    Menu entity done

--- Desktop/Odoo/recipes/models/menu.py
+++ Desktop/Odoo/recipes/models/menu.py
@@ -3,7 +3,7 @@ from odoo import models
 class Menu(models.model):
     __name = 'recipes.menu'
     
-    id = fields.Integer(requiered=True),
-    name = fields.char(requered=True),
-    type = fields.selection([('breakfast','Breakfast'), ('lunch','Lunch'), ('snack','Snack'), ('dinner','Dinner')],'Tipo'),
+    id = fields.Integer(required=True),
+    name = fields.char(required=True),
+    type = fields.selection([('breakfast','Breakfast'), ('lunch','Lunch'), ('snack','Snack'), ('dinner','Dinner')],'Tipo')
 
