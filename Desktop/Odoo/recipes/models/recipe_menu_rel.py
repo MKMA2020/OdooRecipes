c80013f21ffb552e02e6b8cf944cc9436c80c0df
Martin Valiente Ainz <2dam@MACINF01.TartangaLH.eus>
2020-12-21 14:13:32 +0100

    recipe_menu_rel commented.

--- Desktop/Odoo/recipes/models/recipe_menu_rel.py
+++ Desktop/Odoo/recipes/models/recipe_menu_rel.py
@@ -1,10 +1,22 @@
-from odoo import models,fields,api
+from odoo import api
+from odoo import fields
+from odoo import models
 
 class MenuRecipeRel(models.Model):
+    #Relationship class between menu and recipe.
     _name = 'recipes.menureciperel'
     
-    recipe_id = fields.Many2one('recipes.recipe', string='Recipes contained by menu', ondelete='cascade')
-    menu_id = fields.Many2one('recipes.menu', string='Menu that contains', ondelete='cascade')
+    #recipe_id field for the MenuRecipeRel model. Will contain the relation with
+    #the Recipe id that contains the menus.
+    recipe_id = fields.Many2one('recipes.recipe',
+                                string='Recipes contained by menu',
+                                ondelete='cascade')
+    
+    #menu_id field for the MenuRecipeRel model. Will contain the relation with 
+    #the Menu id that contains the recipes.
+    menu_id = fields.Many2one('recipes.menu',
+                              string='Menu that contains',
+                              ondelete='cascade')
    
     menuRecipeType = fields.Selection([
                                       ('starter', 'Starter'),
@@ -13,4 +25,5 @@ class MenuRecipeRel(models.Model):
                                       ('dessert', 'Dessert'),
                                       ('sides', 'Sides'),
                                       ('drink', 'Drinks')
-                                      ],string ='Type',required=True)
+                                      ], string='Type', required=True)
+                                      
