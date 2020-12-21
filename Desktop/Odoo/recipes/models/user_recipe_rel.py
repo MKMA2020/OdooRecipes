bb8b89475a577f04d47f1054732035da6791aaeb
2dam <2dam@MACINF04.TartangaLH.eus>
2020-12-21 14:24:43 +0100

    Commented: user.py user_recipe_rel.py

--- Desktop/Odoo/recipes/models/user_recipe_rel.py
+++ Desktop/Odoo/recipes/models/user_recipe_rel.py
@@ -1,10 +1,13 @@
 from odoo import models,fields,api
 
+# Model of the relation between user and recipe.
 class UserRecipeRel(models.Model):
    _name = 'recipes.userreciperel'
    
    user_id = fields.Many2one('res.users', string='Commenter', ondelete='cascade')
    recipe_id = fields.Many2one('recipes.recipe', string='Recipe commented', ondelete='cascade')
    
+   # Text of a comment on a recipe.
    comment = fields.Char(required=True)
+   # Rating given by a comment on a recipe.
    rating = fields.Float(required=True)
