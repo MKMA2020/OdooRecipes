6634f8e3b1b7f8aa41af6ac4d21ac213eec23085
Martin Valiente Ainz <2dam@MACINF01.TartangaLH.eus>
2021-01-12 08:55:22 +0100

    Views menu done.

--- Desktop/Odoo/recipes/__manifest__.py
+++ Desktop/Odoo/recipes/__manifest__.py
@@ -27,6 +27,7 @@
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
+        'views/recipeViews.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
