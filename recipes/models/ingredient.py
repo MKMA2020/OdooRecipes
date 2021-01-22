7f412d48134100d82b2a448f143366c11189662a
2dam <2dam@MACINF01.TartangaLH.eus>
2021-01-22 14:15:12 +0100

    No funciona nada

--- recipes/models/ingredient.py
+++ recipes/models/ingredient.py
@@ -1,13 +1,15 @@
 from odoo import api
 from odoo import fields
 from odoo import models
+from odoo.exceptions import ValidationError
+from python import re
 
 class Ingredient(models.Model):
     #Odoo model name identification
     _name = 'recipes.ingredient'
     
     #Name field for the Ingredient model. Char and required type.
-    name = fields.Char(required=True)
+    name = fields.Char(required=True, string='Name', domain=[('name', '!=', record.name)])
     
     #IngredientType field for the Ingredient model. Selection and required type.
     ingredientType = fields.Selection([
@@ -29,8 +31,15 @@ class Ingredient(models.Model):
     
     #user_id field for the Ingredient model. Will contain the relation with the 
     #user that created the ingredient.
-    user_id = fields.Many2one('res.users', string='Ingredient creator')
+    user_id = fields.Many2one('res.users', string='Ingredient creator', default=lambda self: self.env.uid)
     
     #recipes field for the Ingredient model. Will contain the relation with the 
     #recipes that contain the ingredients.
     recipes = fields.Many2many('recipes.recipe', string='Part of recipes')
+
+    @api.constrains('name')              
+    def _check_name(self):
+        raise ValidationError("Entra")
+        for record in self:
+            if record.name.isalpha():
+                raise ValidationError("You entered a number in the Name.")
